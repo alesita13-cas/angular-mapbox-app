80f9cd18657411a3984bcd6d6727d83c9e57fcbd
alesita13-cas <castrillo777ale@gmail.com>
2026-03-14 20:11:17 -0600

    Create AppComponent for Angular Mapbox application

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Angular Mapbox App</h1>
+    <app-mapa></app-mapa>
+    <app-listado></app-listado>
+  `
+})
+export class AppComponent {}
